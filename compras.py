87bea2bc58c827fd12e06c7e75b7b9fd98a15c79
Giovana Gezuato <giovanagezuato@gmail.com>
2025-02-13 16:49:14 -0300

    Atualizações

--- compras.py
+++ compras.py
@@ -1,4 +1,5 @@
 from funcionalidades import *
+from funcionalidades.arquivo import *
 
 arq = 'lista.txt'
 
@@ -9,12 +10,21 @@ titulo('LISTA DE COMPRAS MAKER', 42, '=-', verde)
 while True:
     titulo('MENU', 42, '-', azul)
     print('1 - Adicionar itens na lista / 2 - Ver lista / 3 - Sair')
-    resp = int(input('Sua escolha: '))
+    while True:
+        resp = leia_int('Sua escolha: ')
+        if resp in range(0, 5):
+            break
+        else:
+            print('\033[0;31mDigite uma opção válida! [1, 2 ou 3]\033[m')
     match resp:
         case 1:
-            item = str(input('Item: '))
-            quant = int(input('Quantidade:'))
-            adicionar_itens(arq, item, quant)
+            while True:
+                item = str(input('Item: '))
+                quant = int(input('Quantidade:'))
+                adicionar_itens(arq, item, quant)
+                continua = str(input('Deseja adicionar mais itens? [S/N]: ')).upper().strip()[0]
+                if continua == 'N':
+                    break
         case 2:
             ler_arquivo(arq)
         case 3:
--- funcionalidades/__init__.py
+++ funcionalidades/__init__.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 vermelho = '31'
 roxo = '35'
 azul = '36'
@@ -18,59 +16,16 @@ def titulo(msg, tam=42, simbolo='-', cor=vermelho):
     print(f'\033[{cor}m{simbolo}\033[m' * tam)
 
 
-def arquivo_existe(nome_arquivo):
-    try:
-        a = open(nome_arquivo, 'rt')
-        a.close()
-    except FileNotFoundError:
-        return False
-    else:
-        return True
-
-
-def criar_arquivo(nome_arquivo):
-    try:
-        with open(nome_arquivo, 'w', encoding="utf-8") as a:
-            a.write('Índice,Produto,Qtde\n')
-        print(f'Arquivo {nome_arquivo} criado com sucesso!')
-    except Exception as e:
-        print(f'Erro na criação do arquivo: {e}')
-
-
-def ler_arquivo(nome_arquivo):
-    try:
-        with open(nome_arquivo, 'r', encoding='utf-8') as f:
-            linhas = f.readlines()
-        if len(linhas) <= 1:
-            print('A lista de compras está vazia.')
-            return
-
-        tabela = pd.read_csv(nome_arquivo, delimiter=',', encoding="utf-8", index_col=0)
-        print(tabela)
-
-    except FileNotFoundError:
-        print('Erro ao encontrar o arquivo!')
-    except UnicodeDecodeError:
-        print('Erro de codificação!')
-    except Exception as e:
-        print(f'Erro ao ler o arquivo: {e}')
-
-
-def adicionar_itens(nome_arquivo, item, qtde):
-    try:
-        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
-            linhas = arquivo.readlines()
-
-        if len(linhas) > 1:
-            ultimo_indice = int(linhas[-1].split(',')[0])
+def leia_int(msg):
+    while True:
+        try:
+            num = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO! Por favor, digite um número inteiro válido.\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\n\033[0;31mEntrada de dados interrompida pelo usuário!\033[m')
+            return 0
         else:
-            ultimo_indice = 0
-        novo_indice = ultimo_indice + 1
-
-        with open(nome_arquivo, 'a', encoding='utf-8') as a:
-            a.write(f'{novo_indice},{item},{qtde}\n')
-            print(f'Item "{item}" adicionado com sucesso!')
-    except Exception as e:
-        print(f'Erro ao adicionar item: {e}')
-
+            return num
 
